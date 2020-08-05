e0e9c0015baa46b2874c08dc311ad4b98cbb57d1
olakv <kveola13@student.westerdals.no>
2020-08-05 09:33:05 +0200

    Changes to the application

--- src/main/java/com/kveola13/springaop/SpringAopApplication.java
+++ src/main/java/com/kveola13/springaop/SpringAopApplication.java
@@ -1,13 +1,34 @@
 package com.kveola13.springaop;
 
+import com.kveola13.springaop.business.Business;
+import com.kveola13.springaop.business.SecondBusiness;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringAopApplication {
+public class SpringAopApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringAopApplication.class, args);
-	}
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    private Business business;
+
+    @Autowired
+    private SecondBusiness secondBusiness;
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAopApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        logger.info(business.calculateSomething());
+        logger.info(secondBusiness.calculateSomething());
+    }
 
 }
