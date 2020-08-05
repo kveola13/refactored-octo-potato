07d29b08a722ea04ac6a0cb84d966ae8e1b648d5
olakv <kveola13@student.westerdals.no>
2020-08-05 09:32:41 +0200

    Added business

---
+++ src/main/java/com/kveola13/springaop/business/Business.java
@@ -0,0 +1,15 @@
+package com.kveola13.springaop.business;
+
+import com.kveola13.springaop.data.Dao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class Business {
+    @Autowired
+    private Dao dao;
+
+    public String calculateSomething() {
+        return dao.retrieveSomething();
+    }
+}
