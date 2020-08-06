9a4aa7ea43f6658eef656019ff5aabcf4347b0f5
olakv <kveola13@student.westerdals.no>
2020-08-06 08:23:58 +0200

    Changes to common joint point

--- src/main/java/com/kveola13/springaop/aspect/CommonJoinPointConfig.java
+++ src/main/java/com/kveola13/springaop/aspect/CommonJoinPointConfig.java
@@ -3,8 +3,27 @@ package com.kveola13.springaop.aspect;
 import org.aspectj.lang.annotation.Pointcut;
 
 public class CommonJoinPointConfig {
-    @Pointcut("execution(*  com.kveola13.springaop..*.*(..))")
+    @Pointcut("execution(*  com.kveola13.springaop.data.*.*(..))")
     public void dataLayerExecution() {
+
+    }
+
+    @Pointcut("execution(*  com.kveola13.springaop.business.*.*(..))")
+    public void businessLayerExecution() {
+
+    }
+    @Pointcut("com.kveola13.springaop.aspect.CommonJoinPointConfig.dataLayerExecution() && com.kveola13.springaop.aspect.CommonJoinPointConfig.businessLayerExecution()")
+    public void allLayerExecution(){
+
+    }
+
+    @Pointcut("bean(*dao*)")
+    public void namedBeanDao(){
+
+    }
+
+    @Pointcut("within(com.kveola13.springaop.data..*)")
+    public void dataLayerExecutionWithin() {
 
     }
 }
