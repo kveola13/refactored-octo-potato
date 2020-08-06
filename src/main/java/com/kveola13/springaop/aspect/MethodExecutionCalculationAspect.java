860a3f907c36b8c656858894ddf4d180f6c8bd8a
olakv <kveola13@student.westerdals.no>
2020-08-06 08:12:42 +0200

    Added calc aspect

---
+++ src/main/java/com/kveola13/springaop/aspect/MethodExecutionCalculationAspect.java
@@ -0,0 +1,25 @@
+package com.kveola13.springaop.aspect;
+
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+
+@Aspect
+@Configuration
+public class MethodExecutionCalculationAspect {
+
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Around("execution(*  com.kveola13.springaop..*.*(..))")
+    public void around(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = System.currentTimeMillis();
+        joinPoint.proceed();
+        long timeTaken = System.currentTimeMillis() - startTime;
+        logger.info("Time taken by {} is {}", joinPoint, timeTaken);
+    }
+
+}
