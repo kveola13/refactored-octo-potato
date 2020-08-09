a8098cbf376cb77974e4bdfb5455c06a5d231183
olakv <kveola13@student.westerdals.no>
2020-08-09 11:47:29 +0200

    Added Dao

--- src/main/java/com/kveola13/springaop/data/Dao.java
+++ src/main/java/com/kveola13/springaop/data/Dao.java
@@ -1,9 +1,11 @@
 package com.kveola13.springaop.data;
 
+import com.kveola13.springaop.aspect.TrackTime;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class Dao {
+    @TrackTime
     public String retrieveSomething() {
         return "Dao";
     }
