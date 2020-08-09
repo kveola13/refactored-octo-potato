2532405bf168209cedfd6bc92f7b9136609b9746
olakv <kveola13@student.westerdals.no>
2020-08-09 11:47:49 +0200

    Business file

--- src/main/java/com/kveola13/springaop/business/Business.java
+++ src/main/java/com/kveola13/springaop/business/Business.java
@@ -1,5 +1,6 @@
 package com.kveola13.springaop.business;
 
+import com.kveola13.springaop.aspect.TrackTime;
 import com.kveola13.springaop.data.Dao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,7 @@ public class Business {
     @Autowired
     private Dao dao;
 
+    @TrackTime
     public String calculateSomething() {
         return dao.retrieveSomething();
     }
